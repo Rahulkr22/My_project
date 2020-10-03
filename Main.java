326838ab4f9aaf9c9fb35d1421c63d9b54b9106b
Rahulkr22 <66104500+Rahulkr22@users.noreply.github.com>
2020-10-03 11:58:04 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.rahul;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+    }
+}
